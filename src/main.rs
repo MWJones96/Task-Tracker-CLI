c80c6e719a2a9f6d89a974bba51ad3a5ba32b0b3
Matthew Jones <mwjones0896@gmail.com>
2025-10-05 23:20:18 +0100

    Structs defined

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::time::SystemTime;
+
+enum Status {
+    Todo,
+    InProgress,
+    Done,
+}
+
+struct Task {
+    id: u32,
+    description: String,
+    status: Status,
+    created_at: SystemTime,
+    updated_at: SystemTime,
+}
+
 fn main() {
     println!("Hello, world!");
 }
